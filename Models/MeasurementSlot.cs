b8f1117e1215d85a213a5d15721f0bc3f4663a05
Boris Bergman <b.c.bergman@uu.nl>
2021-10-12 14:33:11 +0200

    Database fixed

---
+++ Models/MeasurementSlot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BalanceAval.Models
+{
+    public class MeasurementSlot
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public ICollection<MeasurementRow> MeasurementRows { get; set; }
+    }
+}
